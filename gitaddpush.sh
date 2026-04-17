9e4bc9229faae9d79a0218a3beae423fe291a7d1
gkholman <gkenholman+github@gmail.com>
2026-04-17 09:27:11 -0400

    Tweak git script

--- gitaddpush.sh
+++ gitaddpush.sh
@@ -1,1 +1,1 @@
-pushd ~/u/git/gkholman/GKHmusic.ca ; git add . ; git commit -m "$1" ; git push ; popd
+pushd ~/u/git/gkholman/GKHmusic.ca >/dev/null ; git add . ; git commit -m "$1" ; git push ; popd >/dev/null 
