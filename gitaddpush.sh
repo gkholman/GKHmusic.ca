a01a1199678301ce8867d8d37e28c5fe408c685c
gkholman <gkenholman+github@gmail.com>
2026-04-17 09:30:49 -0400

    Tweak git script

--- gitaddpush.sh
+++ gitaddpush.sh
@@ -1,1 +1,6 @@
+if [ -z "$1" ] || [ -n "$2" ]; then
+  echo "Please supply exactly one argument"
+  exit 1
+fi
+
 pushd ~/u/git/gkholman/GKHmusic.ca >/dev/null ; git add . ; git commit -m "$1" ; git push ; popd >/dev/null 
